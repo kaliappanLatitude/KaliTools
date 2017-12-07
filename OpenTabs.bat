4bcc2991127c75fb9b5d2ba6879699225819b988
Kaliappan <Kaliappan@latitudefintech.com>
2017-12-07 14:03:48 +0530

    Added gmail tab to be opened in OpenTabs file

--- OpenTabs.bat
+++ OpenTabs.bat
@@ -4,3 +4,4 @@ start "Microsoft Online" "https://www.office.com/1/?auth=2&home=1&from=ShellLogo
 start "OneNote Online" "https://latitudefintech-my.sharepoint.com/personal/kaliappan_latitudefintech_com/_layouts/15/WopiFrame.aspx?sourcedoc={768dfb77-e685-49d4-a004-2008158bc55f}&action=edit&wd=target%28%2F%2FAngular%20Learning.one%7Cecc3c66b-d2be-464c-9047-17d9ecda7819%2FDemo%7Cf770894a-cee7-4f2c-a762-fc1ec1e60955%2F%29"
 start "Google" "www.google.com"
 start "RackPackers" "www.rackpackers.com"
+start "gMail" "www.gmail.com"
